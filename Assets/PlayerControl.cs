fbd9826e4644cb4b94c2104943d50f83c06e2562
klefit <z121@vp.pl>
2017-10-06 16:40:07 +0200

    Witam

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
-	public float speed;
+	public float sped;
 
 
 
@@ -15,16 +15,16 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("w")){
-			transform.Translate (Vector3.up*speed);
+			transform.Translate (Vector3.up*sped);
 		}
 		if (Input.GetKey("s")){
-			transform.Translate (Vector3.down*speed);
+			transform.Translate (Vector3.down*sped);
 		}
 		if (Input.GetKey("a")){
-			transform.Translate (Vector3.left*speed);
+			transform.Translate (Vector3.left*sped);
 		}
 		if (Input.GetKey("d")){
-			transform.Translate (Vector3.right*speed);
+			transform.Translate (Vector3.right*sped);
 		}
 
 
