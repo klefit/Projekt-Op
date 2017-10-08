729a6767b298785b0e41536307abe6dc9d381bd3
klefit <z121@vp.pl>
2017-10-08 18:27:16 +0200

    Strzelanie + detekcja ścian

---
+++ Assets/kolizja.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kolizja : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D node){
+		if(node.gameObject.tag == "Wall")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
